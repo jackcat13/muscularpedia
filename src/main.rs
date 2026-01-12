fa11b9edb551acefe2681b0e03d217f9094dec4a
jackcat13 <christophehenry11@gmail.com>
2026-01-12 15:04:59 +0100

    feat: Basic page to render list of exercices mapped by categories

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
-fn main() {
-    println!("Hello, world!");
+use std::collections::HashMap;
+use askama::Template;
+use axum::{Router, response::Html, routing::get};
+use templates::home::HomeTemplate;
+
+use crate::templates::home::{Category, Exercise, Exercises};
+use crate::templates::home::Category::{Back, Core, Legs};
+
+mod templates;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(home));
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+    println!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn home() -> Html<String> {
+    let template = HomeTemplate {
+        exercises: init_exercises(),
+    };
+    Html(template.render().expect("Failed to render home template"))
+}
+
+fn init_exercises() -> std::collections::HashMap<Category, Exercises> {
+    let mut map = HashMap::new();
+    map.insert(Legs, Exercises {
+        items: vec![
+            Exercise {
+                name: "Squat".to_string(),
+                description: "todo".to_string(),
+                image: "todo".to_string(),
+                categories: vec![Legs, Core, Back],
+            }
+        ]
+    });
+    map
 }
---
+++ src/templates/home.rs
@@ -0,0 +1,85 @@
+use serde::{Deserialize, Serialize};
+use std::{collections::HashMap, fmt::Display};
+use std::fmt::Formatter;
+use askama::{Template, filters::HtmlSafe};
+
+#[derive(Template)]
+#[template(path = "home.html")]
+pub struct HomeTemplate {
+    pub exercises: HashMap<Category, Exercises>,
+}
+
+#[derive(Deserialize)]
+pub struct Exercises {
+    pub items: Vec<Exercise>,
+}
+
+impl HtmlSafe for Exercises {}
+
+impl Display for Exercises {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            r#"
+                <div>{}</div>
+            "#,
+            as_exercises_string(&self.items)
+        )
+    }
+}
+
+fn as_exercises_string(exercises: &Vec<Exercise>) -> String {
+    let mut s = String::new();
+    for exercise in exercises {
+        s += &exercise.to_string();
+    }
+    s
+}
+
+#[derive(serde::Deserialize)]
+pub struct Exercise {
+    pub name: String,
+    pub description: String,
+    pub image: String,
+    pub categories: Vec<Category>,
+}
+
+impl HtmlSafe for Exercise {}
+
+impl Display for Exercise {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            r#"
+                <div>{}</div>
+                <div>{}</div>
+                <div>{}</div>
+                <div>{}</div>
+            "#,
+            self.name,
+            self.description,
+            self.image,
+            as_categories_string(&self.categories)
+        )
+    }
+}
+
+fn as_categories_string(categories: &Vec<Category>) -> String {
+    let mut s = String::new();
+    for category in categories {
+        s += &category.to_string();
+    }
+    s
+}
+
+#[derive(Deserialize, Serialize, Hash, PartialEq, Eq, strum::Display)]
+pub enum Category {
+    Shoulder,
+    Chest,
+    Back,
+    Arms,
+    Core,
+    Legs,
+}
+
+impl HtmlSafe for Category {}
